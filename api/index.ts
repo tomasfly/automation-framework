2f7c1c7e879b5399d5ee4dfb49a146d1d7e4bdc4
Tomas Fleiderman <tomas.fleiderman@newsignature.com>
2022-12-26 17:47:58 +0100

    Update branch

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,28 @@
 
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import axios from 'axios';
+
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.send("Welcome to your App!")
-  })
-  
-  app.listen(port, function () {
+})
+
+app.listen(port, function () {
     console.log(`Express server listening on port ${port}`)
-  })
-  
+})
+
+app.get("/users", (req, res) => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+            res.json(response.data)
+        }).catch(function (error) {
+            res.json("Error occured!")
+        })
+})
+
